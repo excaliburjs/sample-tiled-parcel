c222207d8e1f6dc0d1f3166c4cb0c4354d15cb5f
Erik Onarheim <erik.onarheim@gmail.com>
2023-11-08 17:43:22 -0600

    initial commit for parcel tiled plugin

---
+++ files.d.ts
@@ -0,0 +1,14 @@
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.tmx' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.tsx' {
+    const value: string;
+    export default value;
+}
