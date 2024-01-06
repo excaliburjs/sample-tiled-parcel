52e4d2f6091add2acc546039bf34918ef7b6bc55
Erik Onarheim <erik.onarheim@gmail.com>
2024-01-05 22:34:34 -0600

    rev to latest

--- src/resources.ts
+++ src/resources.ts
@@ -1,31 +1,30 @@
 import { ImageFiltering, ImageSource, Loadable, Loader, Resource, TileMap } from "excalibur";
-import { TiledMapResource } from '@excaliburjs/plugin-tiled';
+import { TiledResource } from '@excaliburjs/plugin-tiled';
 
 // Import paths to work with Parcel
 import heroPath from '../img/Solaria Demo Pack Update 03/Solaria Demo Pack Update 03/16x16/Sprites/Hero 01.png';
 import tilesetPath from '../img/Solaria Demo Pack Update 03/Solaria Demo Pack Update 03/16x16/Tilesets/Solaria Demo Update 01.png';
 import tmxPath from '../res/first-level.tmx';
 import tsxPath from '../res/tileset.tsx';
+import { Player } from "./player";
 
 export const Resources = {
     HeroSpriteSheetPng: new ImageSource(heroPath, false, ImageFiltering.Pixel),
-    TiledMap: new TiledMapResource(tmxPath),
-    TsxResource: new Resource(tsxPath, 'text')
-}
-
-// Patch @excalibur/plugin-tile path resolution to work around Parcel
-const convertPath = Resources.TiledMap.convertPath;
-Resources.TiledMap.convertPath = (originalPath: string, relativePath: string) => {
-  if (relativePath.includes('.tmx')) {
-    return tmxPath;
-  }
-  if (relativePath.includes('.tsx')) {
-    return tsxPath;
-  }
-  if (relativePath.includes('.png')) {
-    return tilesetPath;
-  }
-  return convertPath(originalPath, relativePath);
+    TiledMap: new TiledResource(tmxPath, {
+      entityClassNameFactories: {
+        player: (props) => {
+          const player = new Player(props.worldPos);
+          player.z = 100;
+          return player;
+        }
+      },
+      // Path map intercepts and redirects to work around parcel's static bundling
+      pathMap: [
+        { path: 'first-level.tmx', output: tmxPath },
+        { path: 'Solaria Demo Update 01.png', output: tilesetPath },
+        { path: 'tileset.tsx', output: tsxPath }
+      ]
+    })
 }
 
 export const loader = new Loader();
